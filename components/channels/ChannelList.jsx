3ef0e480fb8a57f902c79505a21fed7e967b3ac3
Magnus <magnus@smsgrupp.se>
2018-02-03 15:45:02 +0100

    created basic channel module

---
+++ components/channels/ChannelList.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import Channel from './Channel.jsx'
+
+class ChannelList extends Component {
+	render() {
+		return (
+			<ul>{
+				this.props.channels.map(channel => {
+					return (
+						<Channel 
+							channel={channel}
+							key={channel.id}
+							setChannel={this.props.setChannel}
+						/>
+					)
+				})
+			}</ul>
+		)
+	}
+}
+
+ChannelList.propTypes = {
+	channels: PropTypes.array.isRequired,
+	setChannel: PropTypes.func.isRequired,
+}
+
+export default ChannelList
